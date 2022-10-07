9075bf64f561f1d4235731303b24c4ab7af78347
ghosttester1 <115147905+ghosttester1@users.noreply.github.com>
2022-10-07 08:44:27 +0530

    Update bulk.py

--- main/plugins/bulk.py
+++ main/plugins/bulk.py
@@ -59,8 +59,8 @@ async def _bulk(event):
                 return await conv.send_message("Cannot wait more longer for your response!")
             try:
                 value = int(_range.text)
-                if value > 100:
-                    return await conv.send_message("You can only get upto 100 files in a single batch .")
+                if value > 10000:
+                    return await conv.send_message("You can only get upto 10000 files in a single batch .")
             except ValueError:
                 return await conv.send_message("Range must be an integer!")
             s, r = await check(userbot, Bot, _link)
@@ -84,9 +84,9 @@ async def run_batch(userbot, client, sender, link, _range):
             timer = 15
         if not 't.me/c/' in link:
             if i < 25:
-                timer = 2
+                timer = 1
             else:
-                timer = 3
+                timer = 2
         try:
             await get_bulk_msg(userbot, client, sender, link, i) 
         except FloodWait as fw:
