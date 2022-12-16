e08cc7f70c7190ad88e6c607a0c02a54f50d195b
dileep542352 <118031927+dileep542352@users.noreply.github.com>
2022-12-16 16:09:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
+FORCESUB = config("FORCESUB", default=None)
+AUTH = 
